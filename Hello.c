6f83bfddab29beb4ec2044338d1a0de31325efbc
gshanle <59966064+gshanle@users.noreply.github.com>
2020-01-16 08:41:33 -0600

    Initial commit

---
+++ Hello.c
@@ -0,0 +1,20 @@
+
+
+/*
+Grant Shanle
+1/16/2020
+No collaborators
+This program prints "Hello World!"
+*/
+
+
+#include <stdio.h>
+
+int main(void) {
+	
+	printf("Hello World!")
+
+
+
+return 0;
+}
